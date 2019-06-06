18456a1ec4323b5b5f6177f28c2401247b9bb214
reactor-lee <dongkyu.lee@thingsflow.kr>
2019-06-06 19:31:52 +0900

    [create] project init

---
+++ src/main/java/com/cardgame/gameserver/GameServer.java
@@ -0,0 +1,50 @@
+package com.cardgame.gameserver;
+
+import com.cardgame.gameserver.handler.ChannelDefaultHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class GameServer {
+
+    private int port;
+
+    private GameServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) {
+        int port;
+
+        if(args.length > 0) {
+            port = Integer.parseInt(args[0]); //FIXME: how port configuration? resources?
+        } else  {
+            port = 9988;
+        }
+    }
+
+    private void run() throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class).childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                public void initChannel(SocketChannel ch) {
+                    ch.pipeline().addLast(new ChannelDefaultHandler());
+                }
+            }).option(ChannelOption.SO_BACKLOG, 128).childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
